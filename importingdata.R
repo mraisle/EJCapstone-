90ee2a4271b700fe5c81102c95f02890408912fe
Raisle <mraisle@ad.unc.edu>
2020-02-13 14:00:54 -0500

    g

--- importingdata.R
+++ importingdata.R
@@ -15,9 +15,20 @@ library(here)
 library(dplyr)
 library(plotly)
 library(wesanderson)
+install.packages("googlesheets")
+library(googlesheets)
 
-northhampton <- read.csv(here("beginning/Northampton_data.csv"))
 
 
-try <- read_shape(here("beginning/addresses_NH.shp"))
+
+northhampton <- read.csv(here("geocoded_csv/NH_rawarc.csv"))
+
+
+cleaner <- subset(northhampton, select = -c(Status,Score,Match_type,
+                                      LongLabel,ShortLabel,Addr_type,Type,PlaceName,Place_addr,Phone,
+                                      URL,Rank,AddBldg,AddNum,AddNumFrom,AddNumTo,AddRange,Side,StPreDir,StPreType,StName,StType,StDir,
+                                      BldgName,LevelType,LevelName,UnitType,UnitName,SubAddr,StAddr,Block,Sector,Nbrhd,District,City,
+                                      MetroArea,Territory,Zone,LangCode,Distance,ExInfo,ResultID))
+
+write.csv(cleaner, here("geocoded_csv/NH_cleaner.csv"))
 
