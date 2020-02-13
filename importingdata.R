61c6f46d0f4207db8b814075b4f3e71579fd5ef8
mraisle <mraisle@mraisle-pc.local>
2020-02-13 17:24:48 -0500

    f

--- importingdata.R
+++ importingdata.R
@@ -21,16 +21,31 @@ library(googlesheets)
 
 
 
-northhampton <- read.csv(here("geocoded_csv/NH_cleaner.csv"))
+HERT <- read.csv(here("geocoded_csv/HERT_rawarc.csv"))
 
 
-cleaner <- subset(northhampton, select = -c(Status,Score,Match_type,
+cleaner <- subset(HERT, select = -c(Status,Score,Match_type,
                                       LongLabel,ShortLabel,Addr_type,Type,PlaceName,Place_addr,Phone,
                                       URL,Rank,AddBldg,AddNum,AddNumFrom,AddNumTo,AddRange,Side,StPreDir,StPreType,StName,StType,StDir,
                                       BldgName,LevelType,LevelName,UnitType,UnitName,SubAddr,StAddr,Block,Sector,Nbrhd,District,City,
                                       MetroArea,Territory,Zone,LangCode,Distance,ExInfo,ResultID))
 
-write.csv(cleaner, here("geocoded_csv/NH_cleaner.csv"))
 
+
+cleaner <- subset(cleaner, select = -c(FID,Loc_name))
+
+cleaner <- subset(cleaner, select = -c(BldgType,Xmin,Xmax,Ymin,Ymax))
+
+cleaner <- subset(cleaner, select = -c(IN_Addre_1, IN_Addre_2, IN_Neighbo))
+
+cleaner <- subset(cleaner, select = -c(IN_Subregi,IN_PostalE, IN_Country,USER_lon,USER_lat))
+
+#cleaner$Subregion[16] = "Northampton County"
+
+
+names(cleaner)[names(cleaner)=="Subregion"] <- "County"
+
+
+write.csv(cleaner, here("geocoded_csv/HERT_cleaned.csv"))
 
 
