8677259b35ccd3b2b554d66bd7c75ab4b2679282
Raisle <mraisle@ad.unc.edu>
2020-03-02 11:23:36 -0500

    g

--- checkingvariables.R
+++ checkingvariables.R
@@ -1,3 +1,13 @@
+install.packages("tmaptools")
+install.packageS("ggplot2")
+install.packages("here")
+install.packages("dplyr")
+
+
+
+
+
+
 library(tmaptools)
 library(ggplot2)
 library(here)
@@ -9,7 +19,7 @@ library(wesanderson)
 
 #first let's read in the data 
 
-all <- read.csv(here("geocoded_csv/justvariables_all.csv"))
+all <- read.csv(here("geocoded_csv/justvariables_lesslocations.csv"))
 
 
 #ok so we know that 0 is above detect and 1 is below detect 
@@ -18,6 +28,9 @@ all$Arsenic_Iod == 1
 
 sum(all$Arsenic_Iod == 1, na.rm=TRUE)
 
+
+#we want NA to be excluded from the count 
+
 A0 <- all%>%
   filter(Arsenic_Iod == 0)
 
@@ -25,3 +38,8 @@ A1 <- all%>%
   filter(Arsenic_Iod == 1)
 
 nrow(A1)/(nrow(A1)+nrow(A0))*100
+
+#try to do random # 
+
+runif(1)
+runif(10, min=0, max=.005)
