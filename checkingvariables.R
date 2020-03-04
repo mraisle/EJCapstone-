815e40de5de13b1061d24305cc5d63b253f3d27e
mraisle <mraisle@mraisle-pc.wireless-1x.unc.edu>
2020-03-04 14:27:04 -0500

    random number

--- checkingvariables.R
+++ checkingvariables.R
@@ -3,11 +3,11 @@ install.packageS("ggplot2")
 install.packages("here")
 install.packages("dplyr")
 
+install.packages("tidyr")
 
 
 
-
-
+library(tidyr)
 library(tmaptools)
 library(ggplot2)
 library(here)
@@ -43,3 +43,42 @@ nrow(A1)/(nrow(A1)+nrow(A0))*100
 
 runif(1)
 runif(10, min=0, max=.005)
+
+#select 2 dataframes, first lod 
+
+lod <- all %>%
+  select(c(1,seq(43,97,2)))
+
+lod_long <- lod %>%
+  gather(chemical,lod,-X.1)
+
+
+#now with the data itself 
+
+data <- all %>%
+  select(c(1,seq(42,96,2)))
+
+data_long <- data %>%
+  gather(chemical, data, -X.1)
+
+#combine data 
+
+together <- cbind(data_long,lod_long)
+
+
+togethersub <- together[,c(1,2,3,6)]
+
+
+random <- togethersub %>%
+  mutate("estimate"= ifelse(lod== 1, runif(nrow(togethersub), min=0, max=togethersub$data),togethersub$data))
+
+#trying to plot to make sure it looks ok 
+
+plot <- plot(random)+
+  + geom_bar(aes(x=data,y=estimate))
+plot
+
+
+#need to put the data back in a regular format 
+#try making a new column with x.1 and chemical and combine them back this way 
+  
