f4bf599355805ccc83eec18a09ee1e6379aac03e
mraisle <mraisle@mraisle-pc.local>
2020-04-07 10:12:13 -0400

    f

--- classtoday_4-7.R
+++ classtoday_4-7.R
@@ -22,3 +22,24 @@ EJ <- read.csv(here("4-07forclass.csv"))
 #colnames(EJ)[31] <- "insideoutsidebin"
 
 #write.csv(EJ, here("4-07forclass.csv"))
+
+
+#Basic Stats 
+
+EJ$minoritybin <- as.numeric(EJ$minoritybin)
+EJ$lowincomebin <- as.numeric(EJ$lowincomebin)
+EJ$Edubin <- as.numeric(EJ$Edubin)
+EJ$insideoutsidebin <- as.numeric(EJ$insideoutsidebin)
+
+maxminor <- max(EJ$minoritybin, na.rm=TRUE)
+maxminor
+
+minminor <- min(EJ$minoritybin, na.rm=TRUE)
+minminor
+
+averageminor <- mean(EJ$minoritybin, na.rm=TRUE)
+averageminor
+
+#countif 
+
+percentminority <- count
