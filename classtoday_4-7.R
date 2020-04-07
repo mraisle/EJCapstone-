f7eef771b08ac63b6540b63343a73af4d1c28419
mraisle <mraisle@mraisle-pc.local>
2020-04-07 10:00:06 -0400

    f

---
+++ classtoday_4-7.R
@@ -0,0 +1,16 @@
+#working with binary variables 
+
+#install.packages("ggpubr")
+
+
+library(here)
+library(ggplot2)
+library(tidyr)
+library(dplyr)
+library(wesanderson)
+library(viridis)
+library(grid)
+library(gridExtra)
+library(ggpubr)
+
+#get out data 
